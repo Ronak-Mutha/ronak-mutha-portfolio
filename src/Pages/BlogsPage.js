9e2e518bd2f7fd2095b2f8294fc377285fa86376
Ronak Mutha <mutharonak97@gmail.com>
2021-11-30 15:19:33 +0530

    update blogs page added blog links

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,7 +1,78 @@
 import React from "react";
+import styled from "styled-components";
+import Title from "../Components/Title";
+import blogs from "../data/blogs";
+import { MainLayout, InnerLayout } from "../styles/Layouts";
 
 function BlogsPage() {
-  return <div>Blogs Page</div>;
+  return (
+    <MainLayout>
+      <BlogsStyled>
+        <Title title={"Blogs"} span={"Blogs"} />
+        <InnerLayout className={"blog"}>
+          {blogs.map((blog) => {
+            return (
+              <div key={blog.id} className={"blog-item"}>
+                <div className="image">
+                  <img src={blog.image} alt="" />
+                </div>
+                <small className="creation-date">{blog.creation_date}</small>
+                <div className="title">
+                  <a href={blog.link} target="_blank" rel="noopener noreferrer">
+                    {blog.title}
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </InnerLayout>
+      </BlogsStyled>
+    </MainLayout>
+  );
 }
+
+const BlogsStyled = styled.div`
+  .blog {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 3rem 2rem;
+    .blog-item {
+      background-color: var(--background-dark-grey);
+      padding: 1rem 1rem;
+    }
+    .image {
+      width: 100%;
+      overflow: hidden;
+      padding-bottom: 0.5rem;
+      img {
+        width: 100%;
+        height: 90%;
+        object-fit: cover;
+        transition: all 0.4s ease-in-out;
+        &:hover {
+          cursor: pointer;
+          transform: rotate(3deg) scale(1.1);
+        }
+      }
+    }
+    .title {
+      a {
+        font-size: 1.5rem;
+        padding: 2rem 0;
+        color: var(--white-color);
+        cursor: pointer;
+        transition: all 0.4s ease-in-out;
+        &:hover {
+          color: var(--primary-color);
+        }
+      }
+    }
+    .creation-date {
+      font-size: 0.8rem;
+      display: block;
+      margin-bottom: 1rem;
+    }
+  }
+`;
 
 export default BlogsPage;
---
+++ src/data/blogs.js
@@ -0,0 +1,22 @@
+import blog1 from "../img/blogs/blog1.jpg";
+import blog2 from "../img/blogs/blog2.jpg";
+
+const blogs = [
+  {
+    id: 1,
+    title: "Ultimate guide for Emmet: Speed up your html workflow",
+    creation_date: "22 September 2021",
+    image: blog1,
+    link: "https://ronak-mutha.netlify.app/blogs/blog-01.html",
+  },
+  {
+    id: 2,
+    title:
+      "JavaScript Arrays and its methods: Still a Better Love Story than Twilight",
+    creation_date: "23 September 2021",
+    image: blog2,
+    link: "https://ronak-mutha.netlify.app/blogs/blog-02.html",
+  },
+];
+
+export default blogs;
