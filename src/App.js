b8bbf7b6f8cc69f09359441ac06baba44515b41f
Ronak Mutha <mutharonak97@gmail.com>
2021-12-01 21:07:39 +0530

    Added Dark Mode and media queries

--- src/App.js
+++ src/App.js
@@ -1,34 +1,78 @@
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Sidebar from "./Components/Sidebar";
 import HomePage from "./Pages/HomePage";
-import { Route, Routes } from "react-router"
-import AboutPage from './Pages/AboutPage';
-import ResumePage from './Pages/ResumePage';
-import ProjectsPage from './Pages/ProjectsPage';
-import BlogsPage from './Pages/BlogsPage';
-import ContactPage from './Pages/ContactPage';
+import { Route, Routes } from "react-router";
+import AboutPage from "./Pages/AboutPage";
+import ResumePage from "./Pages/ResumePage";
+import ProjectsPage from "./Pages/ProjectsPage";
+import BlogsPage from "./Pages/BlogsPage";
+import ContactPage from "./Pages/ContactPage";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import MenuIcon from "@mui/icons-material/Menu";
+import Switch from "@mui/material/Switch";
+import { IconButton } from "@mui/material";
 
 function App() {
+  const [theme, setTheme] = useState("dark-theme");
+  const [checked, setChecked] = useState(false);
+  const [navToggle, setNavToggle] = useState(false);
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+  }, [theme]);
+
+  const themeToggler = () => {
+    if (theme === "light-theme") {
+      setTheme("dark-theme");
+      setChecked(false);
+    } else {
+      setTheme("light-theme");
+      setChecked(true);
+    }
+  };
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar navToggle={navToggle} />
+
+      <div className="theme">
+        <div className="light-dark-mode">
+          <div className="left-content">
+            <Brightness4Icon />
+          </div>
+          <div className="right-content">
+            <Switch
+              value=""
+              checked={checked}
+              inputProps={{ "aria-label": "" }}
+              size="medium"
+              onClick={themeToggler}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="ham-burger-menu">
+        <IconButton onClick={() => setNavToggle(!navToggle)}>
+          <MenuIcon />
+        </IconButton>
+      </div>
+
       <MainContentStyled>
-        {/* <div className="lines">
+        <div className="lines">
           <div className="line-1"></div>
           <div className="line-2"></div>
           <div className="line-3"></div>
           <div className="line-4"></div>
-        </div> */}
-        
+        </div>
         <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/resume" element={<ResumePage />} />
-        <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/blogs" element={<BlogsPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/resume" element={<ResumePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/blogs" element={<BlogsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
       </MainContentStyled>
     </div>
   );
@@ -40,17 +84,20 @@ const MainContentStyled = styled.main`
   min-height: 100vh;
 
   .lines {
-    position: absolute:
-    min-height:100%;
+    position: absolute;
+    min-height: 100%;
     width: 100%;
     display: flex;
     justify-content: space-evenly;
-    opacity:0.4;
-    
-    .line-1,.line-2,.line-3,.line-4 {
-      width:1px;
+    opacity: 0.4;
+
+    .line-1,
+    .line-2,
+    .line-3,
+    .line-4 {
+      width: 1px;
       min-height: 100vh;
-      background-color:var(--border-color);
+      background-color: var(--border-color);
     }
   }
 `;
