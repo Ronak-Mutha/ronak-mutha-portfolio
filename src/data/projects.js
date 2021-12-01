b1f43e8c38848cfa33259d49d28b057836b3e1eb
Ronak Mutha <mutharonak97@gmail.com>
2021-12-01 20:38:05 +0530

    Updated Projects Page

---
+++ src/data/projects.js
@@ -0,0 +1,26 @@
+import project from "../assets/img/avatar.jpg";
+
+const portfolios = [
+  {
+    id: 1,
+    category: "React",
+    image: project,
+    github: "https://www.github.com",
+    wesite: "https://www.github.com",
+    title: "Portfolio",
+    text: "Portfolio made with React",
+    alt: "React Project",
+  },
+  {
+    id: 2,
+    category: "JavaScript",
+    image: project,
+    github: "https://www.github.com",
+    wesite: "https://www.github.com",
+    title: "Neogcamp portfolio",
+    text: "Portfolio for Neogcamp made with Javascript",
+    alt: "Javascript Project",
+  },
+];
+
+export default portfolios;
