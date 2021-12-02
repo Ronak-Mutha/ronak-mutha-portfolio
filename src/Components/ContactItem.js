53125ee1dd1695455ac60aa8130fea7bc5184960
Ronak Mutha <mutharonak97@gmail.com>
2021-12-02 19:09:45 +0530

    Add styling for contact links

--- src/Components/ContactItem.js
+++ src/Components/ContactItem.js
@@ -1,57 +1,59 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-
-function ContactItem({title, icon, contact, link}) {
-    return (
-        <ContactItemStyled>
-            <div className="left-content">
-                {
-                    icon
-                }
-            </div>
-            <div className="right-content">
-                <h6>{title}</h6>
-                {!link && <p>{contact}</p>}
-                {link && <a href={link}>{contact}</a>}
-            </div>
-        </ContactItemStyled>
-    )
+function ContactItem({ title, icon, contact, link }) {
+  return (
+    <ContactItemStyled>
+      <div className="left-content">{icon}</div>
+      <div className="right-content">
+        <h6>{title}</h6>
+        {!link && <p>{contact}</p>}
+        {link && (
+          <a className="contact-link" href={link}>
+            {contact}
+          </a>
+        )}
+      </div>
+    </ContactItemStyled>
+  );
 }
 
 const ContactItemStyled = styled.div`
-    padding:1.5rem 2rem;
-    background-color: var(--background-dark-grey);
+  padding: 1.5rem 2rem;
+  background-color: var(--background-dark-grey);
+  display: flex;
+  align-items: center;
+  @media screen and (max-width: 502px) {
+    width: 95%;
+  }
+  &:not(:last-child) {
+    margin-bottom: 2.5rem;
+  }
+  .left-content {
+    padding: 1.5rem;
+    border: 1px solid var(--border-color);
+    font-size: 2rem;
     display: flex;
     align-items: center;
-    @media screen and (max-width: 502px) {
-        width: 95%;
-      }
-    &:not(:last-child){
-        margin-bottom: 2.5rem;
+    justify-content: center;
+    margin-right: 1.5rem;
+    svg {
+      font-size: 2.3rem;
     }
-    .left-content{
-        padding: 1.5rem;
-        border: 1px solid var(--border-color);
-        font-size: 2rem;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin-right: 1.5rem;
-        svg{
-            font-size: 2.3rem;
-        }
+  }
+  .right-content {
+    h6 {
+      color: var(--white-color);
+      font-size: 1.2rem;
+      padding-bottom: 0.6rem;
     }
-    .right-content{
-        h6{
-            color: var(--white-color);
-            font-size: 1.2rem;
-            padding-bottom: .6rem;
-        }
-        p{
-            padding: .1rem 0;
-        }
+    p {
+      padding: 0.1rem 0;
     }
+    .contact-link {
+      text-decoration: underline;
+    }
+  }
 `;
 
 export default ContactItem;
